7879b64683117de19cd6bc5e278c665cf249efcc
Pavel <strikecool1@gmail.com>
2022-02-21 21:05:42 +0300

    update data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,32 +16,36 @@ namespace TEST
 
             
         }
-    private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
-    {
-        var updateInfo = await manager.CheckForUpdate();
-
-        if (updateInfo.ReleasesToApply.Count > 0)
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
         {
-                UpdateButton.IsEnabled = true;
+            var updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                    UpdateButton.IsEnabled = true;
+            }
+            else
+            {
+                    UpdateButton.IsEnabled = false;
+            }
         }
-        else
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-                UpdateButton.IsEnabled = false;
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated succesfuly!");
         }
-    }
 
-    private async void UpdateButton_Click(object sender, RoutedEventArgs e)
-    {
-        await manager.UpdateApp();
-
-        MessageBox.Show("Updated succesfuly!");
-    }
-
-    private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/DefPavel/TEST");
 
             CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
-        }
+
+        }  
+           
+     
+
     }
 }

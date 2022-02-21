191f6a3464ccd83a912fd76112ad52bf69dd6b77
Pavel <strikecool1@gmail.com>
2022-02-21 20:33:55 +0300

    fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,13 +21,40 @@ namespace TEST
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UpdateManager manager;
+
         public MainWindow()
         {
             InitializeComponent();
 
             
         }
+    private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+    {
+        var updateInfo = await manager.CheckForUpdate();
 
-        
+        if (updateInfo.ReleasesToApply.Count > 0)
+        {
+                UpdateButton.IsEnabled = true;
+        }
+        else
+        {
+                UpdateButton.IsEnabled = false;
+        }
+    }
+
+    private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+    {
+        await manager.UpdateApp();
+
+        MessageBox.Show("Updated succesfuly!");
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/DefPavel/TEST");
+
+            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
     }
 }
